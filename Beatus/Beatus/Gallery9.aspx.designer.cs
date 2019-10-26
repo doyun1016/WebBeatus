f5cb9df43cb270bddbc29d238544826330d2b691
doyun1016 <54304971+doyun1016@users.noreply.github.com>
2019-10-26 21:17:52 +0900

    Gallery9 html,css

--- Beatus/Beatus/Gallery9.aspx.designer.cs
+++ Beatus/Beatus/Gallery9.aspx.designer.cs
@@ -11,7 +11,16 @@ namespace Beatus
 {
 
 
-    public partial class _Default
+    public partial class webform1
     {
+
+        /// <summary>
+        /// form1 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
